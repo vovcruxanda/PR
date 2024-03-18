fe864c707a5a14061d9d48d7b3504c9b33cf4b4e
Vovc Ruxanda <ruxanda.vovc1@gmail.com>
2024-03-18 19:23:23 +0200

    Lab2

---
+++ Lab2/client/client/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter server IP address: ");
+        string serverIP = Console.ReadLine();
+        int PORT = 8000;
+
+        Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+        Console.Write("Enter your name: ");
+        string name = Console.ReadLine();
+
+        clientSocket.SendTo(Encoding.ASCII.GetBytes("JOIN " + name), new IPEndPoint(IPAddress.Parse(serverIP), PORT));
+
+        Thread receiveThread = new Thread(ReceiveMessages);
+        receiveThread.Start(clientSocket);
+
+        while (true)
+        {
+            string user_input = Console.ReadLine();
+
+            if (user_input.StartsWith("/msg "))
+            {
+                string[] input_parts = user_input.Substring(5).Split(new char[] { ' ' }, 2);
+                if (input_parts.Length != 2)
+                {
+                    Console.WriteLine("Invalid private message format. Use /msg <recipient> <message>");
+                    continue;
+                }
+                string recipient_name = input_parts[0];
+                string message_content = input_parts[1];
+
+                clientSocket.SendTo(Encoding.ASCII.GetBytes("PRIVMSG " + recipient_name + " " + message_content), new IPEndPoint(IPAddress.Parse(serverIP), PORT));
+            }
+            else
+            {
+                clientSocket.SendTo(Encoding.ASCII.GetBytes("BROADCAST " + user_input), new IPEndPoint(IPAddress.Parse(serverIP), PORT));
+            }
+        }
+    }
+
+    static void ReceiveMessages(object clientSocket)
+    {
+        Socket udpClient = (Socket)clientSocket;
+
+        while (true)
+        {
+            byte[] buffer = new byte[1024];
+            EndPoint serverEP = new IPEndPoint(IPAddress.Any, 0);
+            int bytesRead = udpClient.ReceiveFrom(buffer, ref serverEP);
+            Console.WriteLine(Encoding.ASCII.GetString(buffer, 0, bytesRead));
+        }
+    }
+}
---
+++ Lab2/server/server/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        int PORT = 8000;
+
+        Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        serverSocket.Bind(new IPEndPoint(IPAddress.Any, PORT));
+
+        var clients = new Dictionary<IPEndPoint, string>();
+
+        Console.WriteLine("Server started...");
+
+        while (true)
+        {
+            EndPoint clientAddress = new IPEndPoint(IPAddress.Any, 0);
+            byte[] buffer = new byte[1024];
+            int bytesRead = serverSocket.ReceiveFrom(buffer, ref clientAddress);
+
+            if (!clients.ContainsKey((IPEndPoint)clientAddress))
+            {
+                string name = Encoding.ASCII.GetString(buffer, 0, bytesRead).Split()[1];
+                clients[(IPEndPoint)clientAddress] = name;
+                Console.WriteLine("Client {0} joined the chat.", name);
+            }
+            else if (Encoding.ASCII.GetString(buffer, 0, bytesRead).StartsWith("PRIVMSG "))
+            {
+                string input = Encoding.ASCII.GetString(buffer, 0, bytesRead).Substring(8);
+                string[] inputParts = input.Split(new[] { ' ' }, 2);
+                if (inputParts.Length != 2)
+                {
+                    Console.WriteLine("Invalid private message format. Use PRIVMSG <recipient> <message>");
+                    continue;
+                }
+                string recipientName = inputParts[0];
+                string messageContent = inputParts[1];
+                IPEndPoint recipientAddress = null;
+                foreach (var client in clients)
+                {
+                    if (client.Value == recipientName)
+                    {
+                        recipientAddress = client.Key;
+                        break;
+                    }
+                }
+                if (recipientAddress == null)
+                {
+                    Console.WriteLine("SERVER: No client with the name {0}.", recipientName);
+                }
+                else
+                {
+                    string senderName = clients[(IPEndPoint)clientAddress];
+                    byte[] privateMessage = Encoding.ASCII.GetBytes(senderName + " sent in PRIVATE: " + messageContent);
+                    serverSocket.SendTo(privateMessage, recipientAddress);
+                }
+            }
+            else if (Encoding.ASCII.GetString(buffer, 0, bytesRead).StartsWith("BROADCAST "))
+            {
+                string senderName = clients[(IPEndPoint)clientAddress];
+                string broadcastMessage = senderName + " BROADCASTED: " + Encoding.ASCII.GetString(buffer, 0, bytesRead).Substring(10);
+                byte[] broadcast = Encoding.ASCII.GetBytes(broadcastMessage);
+                Console.WriteLine(broadcastMessage);
+                foreach (var client in clients)
+                {
+                    if (!client.Key.Equals(clientAddress))
+                    {
+                        serverSocket.SendTo(broadcast, client.Key);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid message format: {0}", Encoding.ASCII.GetString(buffer, 0, bytesRead));
+            }
+        }
+    }
+}
